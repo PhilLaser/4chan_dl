c473369f41ef722b18a8929d5d4dddcf087136b1
marsh <marshmallow@marshs-MacBook-Pro.local>
2018-11-01 15:55:27 +0100

    fix python's 'use () for print statement' error

--- 4chan_dl.py
+++ 4chan_dl.py
@@ -13,7 +13,7 @@ def get_thread():
 
 if __name__ == '__main__':
     if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-        print "Usage: 4chan_dl.py <ThreadURL> <path_where_to_store_the_files>"
+        print ("Usage: 4chan_dl.py <ThreadURL> <path_where_to_store_the_files>")
     else:
         url = sys.argv[1]  # url to parse
         file_path = sys.argv[2]  # path
